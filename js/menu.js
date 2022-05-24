555a14ee6f28fe7eb8ffbf39324647b06e402fd7
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-05-24 15:48:20 +0300

    заканчивает хэдэр для трёх размеров

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,17 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open-button"),
     closeMenuBtn: document.querySelector(".menu-close-button"),
-    menu: document.querySelector(".mobile-menu"),
+      menu: document.querySelector(".mobile-menu"),
+      // body: document.querySelector("body"),
+      
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-open");
+      refs.menu.classList.toggle("is-open");
+      // refs.body.classList.toggle("no-scroll");
+      
   }
 })();
